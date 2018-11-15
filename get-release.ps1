ed3584eadfab303886b2db7e1d8b068186aa84b3
Mark Lopez <m@silvenga.com>
2018-11-14 19:04:03 -0600

    Added tagging to release creation script.

--- get-release.ps1
+++ get-release.ps1
@@ -1,29 +1,41 @@
 param(
-    [parameter(Mandatory = $true)]
-    [string] $Version
+  [parameter(Mandatory = $true)]
+  [string] $Version
 )
-
-Write-Host "Searching for the $Version release."
 
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12;
 
-$hashUrl = "https://releases.hashicorp.com/consul/" + $Version + "/consul_" + $Version + "_SHA256SUMS"
-
-$response = Invoke-WebRequest $hashUrl
-
-if (-not $response.StatusCode -eq "200")
+Write-Host "Checking for sanity."
+git diff-index --quiet HEAD --
+$clean = $LASTEXITCODE -eq 0
+if (-not $clean)
 {
-    throw "Version was not found."
+  throw "Repository is dirty, will not continue."
+}
+
+Write-Host "Searching for the $Version release."
+
+$hashUrl = "https://releases.hashicorp.com/consul/" + $Version + "/consul_" + $Version + "_SHA256SUMS"
+$response = Invoke-WebRequest $hashUrl
+$downloadSuccess = $response.StatusCode -eq "200"
+if (-not $downloadSuccess)
+{
+  throw "Version was not found."
 }
 
 $content = $response.Content
 $hash = ($content.Split("`n") | Where-Object { $_ -like "*_windows_amd64.zip" }).Split(" ") | Select-Object -First 1
 
-$release = @{ 
-    Version = $Version
-    Hash    = $hash
+$release = @{
+  Version = $Version
+  Hash    = $hash
 } | ConvertTo-Json
 
 $release | Out-File -FilePath "./release.json"
 
 Write-Host "Release file updated."
+
+Write-Host "Creating git commit and tag."
+
+git commit -a -m "Upstream release $Version."
+git tag $Version
